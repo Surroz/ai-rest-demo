f599ada97611dde3e121dcbcee5949e4f849c957
Surroz <breakingbad979@gmail.com>
2026-02-07 22:54:20 +0200

    add text dubbing logic

--- src/main/java/org/surro/springaidemo/controller/AudioToTextController.java
+++ src/main/java/org/surro/springaidemo/controller/AudioToTextController.java
@@ -3,6 +3,11 @@ package org.surro.springaidemo.controller;
 import org.springframework.ai.audio.transcription.AudioTranscriptionOptions;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.audio.transcription.TranscriptionModel;
+import org.springframework.ai.audio.tts.TextToSpeechModel;
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
+import org.springframework.ai.openai.OpenAiAudioSpeechModel;
+import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
 import org.springframework.ai.openai.api.OpenAiAudioApi;
@@ -16,11 +21,13 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController()
 @RequestMapping("/api/audio")
 public class AudioToTextController {
-    TranscriptionModel transcriptionModel;
+    private TranscriptionModel transcriptionModel;
+    private TextToSpeechModel speechModel;
 
     @Autowired
-    public AudioToTextController(OpenAiAudioTranscriptionModel transcriptionModel) {
+    public AudioToTextController(OpenAiAudioTranscriptionModel transcriptionModel, OpenAiAudioSpeechModel speechModel) {
         this.transcriptionModel = transcriptionModel;
+        this.speechModel = speechModel;
     }
 
     @PostMapping("/record")
@@ -32,5 +39,18 @@ public class AudioToTextController {
                 .build();
         AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(file.getResource(), transcriptionOptions);
         return transcriptionModel.call(prompt).getResult().getOutput();
+    }
+
+    @PostMapping("/dubbing")
+    public byte[] textToVoice(@RequestParam String text) {
+        var speechOptions = OpenAiAudioSpeechOptions.builder()
+                .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3)
+                .speed(1.5)
+                .model(OpenAiAudioApi.TtsModel.GPT_4_O_MINI_TTS.value)
+                .voice(OpenAiAudioApi.SpeechRequest.Voice.ALLOY)
+                .build();
+        TextToSpeechPrompt speechPrompt = new TextToSpeechPrompt(text, speechOptions);
+        TextToSpeechResponse response = speechModel.call(speechPrompt);
+        return response.getResult().getOutput();
     }
 }
